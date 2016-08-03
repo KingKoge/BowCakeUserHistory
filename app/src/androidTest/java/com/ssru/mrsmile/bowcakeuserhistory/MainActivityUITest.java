8520aae316b5d46710a32a327d06ad0616dff0cc
kingkoge <ping.comsci@gmail.com>
2016-08-03 23:19:17 +0700

    Create Project

---
+++ app/src/androidTest/java/com/ssru/mrsmile/bowcakeuserhistory/MainActivityUITest.java
@@ -0,0 +1,19 @@
+package com.ssru.mrsmile.bowcakeuserhistory;
+
+
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Created by Mr.Smile on 8/2/2016.
+ */
+public class MainActivityUITest {
+
+    @Test
+    public void sayHi() {
+    }
+
+}
