9376187340b71af372444f9176fe0d0c8bba29b9
kingkoge <ping.comsci@gmail.com>
2016-08-03 23:32:45 +0700

    Add BasePresenter And BaseView

---
+++ app/src/main/java/com/ssru/mrsmile/bowcakeuserhistory/common/BasePresenter.java
@@ -0,0 +1,11 @@
+package com.ssru.mrsmile.bowcakeuserhistory.common;
+
+/**
+ * Created by Mr.Smile on 8/3/2016.
+ */
+public interface BasePresenter {
+
+    void start();
+
+    void stop();
+}
---
+++ app/src/main/java/com/ssru/mrsmile/bowcakeuserhistory/common/BaseView.java
@@ -0,0 +1,9 @@
+package com.ssru.mrsmile.bowcakeuserhistory.common;
+
+/**
+ * Created by Mr.Smile on 8/3/2016.
+ */
+public interface BaseView<T> {
+
+    void setPresenter(T presenter);
+}
