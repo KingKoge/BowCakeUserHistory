50bb4a25a1823548340d103978256b9350c5a4f2
Suttichai <ping.comsci@gmail.com>
2016-09-29 17:35:27 +0700

    bindView fab and add event onclick openAddMemberActivity

--- app/src/main/java/com/ssru/mrsmile/bowcakeuserhistory/main/MainActivity.java
+++ app/src/main/java/com/ssru/mrsmile/bowcakeuserhistory/main/MainActivity.java
@@ -1,11 +1,16 @@
 package com.ssru.mrsmile.bowcakeuserhistory.main;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.view.View;
 
 import com.ssru.mrsmile.bowcakeuserhistory.R;
+import com.ssru.mrsmile.bowcakeuserhistory.member.add.AddMemberActivity;
 import com.ssru.mrsmile.bowcakeuserhistory.mvp.NextzyMvpActivity;
 
-public class MainActivity extends NextzyMvpActivity<MainContractor.Presenter> implements MainContractor.View {
+public class MainActivity extends NextzyMvpActivity<MainContractor.Presenter> implements MainContractor.View, View.OnClickListener {
+    private FloatingActionButton fabAddMember;
 
     @Override
     public int getLayoutView() {
@@ -19,12 +24,12 @@ public class MainActivity extends NextzyMvpActivity<MainContractor.Presenter> im
 
     @Override
     public void bindView() {
-
+        fabAddMember = (FloatingActionButton) findViewById(R.id.main_fab_add_member);
     }
 
     @Override
     public void setupView() {
-
+        fabAddMember.setOnClickListener(this);
     }
 
     @Override
@@ -55,5 +60,21 @@ public class MainActivity extends NextzyMvpActivity<MainContractor.Presenter> im
     @Override
     public void setPresenter(MainContractor.Presenter presenter) {
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == fabAddMember) {
+            addMember();
+        }
+    }
+
+    private void addMember() {
+        onOpenActivity(AddMemberActivity.class);
+    }
+
+    private void onOpenActivity(Class cls) {
+        Intent intent = new Intent(this, cls);
+        startActivity(intent);
     }
 }
