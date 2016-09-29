c419b1cdc1e78c593ddb131e2a9f67cbb8f525a7
Suttichai <ping.comsci@gmail.com>
2016-09-29 17:34:42 +0700

    add EditMember Activity

---
+++ app/src/main/java/com/ssru/mrsmile/bowcakeuserhistory/member/edit/EditMemberActivity.java
@@ -0,0 +1,53 @@
+package com.ssru.mrsmile.bowcakeuserhistory.member.edit;
+
+import android.os.Bundle;
+
+import com.ssru.mrsmile.bowcakeuserhistory.R;
+import com.ssru.mrsmile.bowcakeuserhistory.mvp.NextzyMvpActivity;
+
+public class EditMemberActivity extends NextzyMvpActivity<EditMemberContractor.Presenter> implements EditMemberContractor.View {
+    @Override
+    public int getLayoutView() {
+        return R.layout.activity_edit_member;
+    }
+
+    @Override
+    protected void createPresenter() {
+
+    }
+
+    @Override
+    public void bindView() {
+
+    }
+
+    @Override
+    public void setupView() {
+
+    }
+
+    @Override
+    public void restoreArgument(Bundle bundle) {
+
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void restoreInstanceState(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    public void restoreView() {
+
+    }
+
+    @Override
+    public void saveInstanceState(Bundle outState) {
+
+    }
+}
